ee54088b357bf8c126cb67a59378641e008ad476
zhu <huanghong0118@126.com>
2021-06-10 15:48:37 +0800

    新增mall

---
+++ mall-mbg/src/test/java/com/zhh/mallmbg/MallMbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhh.mallmbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
